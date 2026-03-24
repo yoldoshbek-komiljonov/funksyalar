9f1a313dec74fa492de8da2f9884392faca42520
Yo'ldoshbek_Komiljonov <komiljonov361@gmail.com>
2026-03-24 17:41:32 +0500

    Add files via upload

---
+++ Funksyalar/app.js
@@ -0,0 +1,24 @@
+// Funktion  
+// JS da funksyalar 'funktion' kalit so'zi bn bowlanadi  
+// parametr - funksiya kiruvchi ma'lumot - input  
+// input - kiruvci
+// output - chiquvchi
+// return  - natija qaytariw
+// returndan keyin hechj qanday kod yiziw mmkn emas
+// returndan keyin yozilgan kodlar iwlamaydi
+
+// Funktion declaration
+function myFn(a) {   //input
+    return a * 2   //output
+}
+
+console.log(myFn(10));
+
+function nameLength(name) {
+    return name.length
+}
+
+console.log(nameLength('Behruz'));
+console.log(nameLength('Siroj'));
+console.log(nameLength('Dilnoza'));
+
